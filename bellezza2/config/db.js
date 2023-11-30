7d6953b5fdae3b12334b9ec8ac6eabbf48c6dc25
ayishaaa <ayshanihalaum@gmail.com>
2023-11-30 11:13:30 +0530

    tenth commit

--- bellezza2/config/db.js
+++ bellezza2/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://0.0.0.0:27017/bellezza', {
+    await mongoose.connect('mongodb://localhost:27017/bellezza', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
